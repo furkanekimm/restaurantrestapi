768bc8ab61e7a56b121648c3349f26141220b607
furkanekimm <69049345+furkanekimm@users.noreply.github.com>
2020-11-17 19:56:58 +0300

    Initial commit

---
+++ src/main/java/com/example/restaurantapi/RestaurantapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantapiApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/example/restaurantapi/RestaurantapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurantapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
